bfe9af2c0130a3b15e3625a6749799778af90823
Tanzim <tanzimr@mun.ca>
2026-03-09 00:36:53 -0230

    feat: add admin routes - dashboard, blogs, comments

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,12 @@
 import express from 'express' 
-import { adminLogin } from '../controllers/adminController.js'
-
+import { adminLogin,getAllblogsAdmin ,getAllComments,getDashboard,deleteCommmentById,approveCommmentById} from '../controllers/adminController.js'
+import auth from '../middleware/auth.js'
 const adminRouter = express.Router()
 adminRouter.post('/login', adminLogin)
+adminRouter.get('/all-blogs', auth, getAllblogsAdmin)
+adminRouter.get('/all-comments', auth, getAllComments)
+adminRouter.get('/dashboard', auth, getDashboard)
+adminRouter.post('/delete-comment', auth, deleteCommmentById)
+adminRouter.post('/approve-comment', auth, approveCommmentById)
 
 export default adminRouter;
