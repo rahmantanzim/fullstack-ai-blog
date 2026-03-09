22c7c80d49e866020a0ee8ec0f51e58aa2e128aa
Tanzim <tanzimr@mun.ca>
2026-03-09 01:32:41 -0230

    feat: wrap main.jsx with the AppProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AppProvider } from './context/AppContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
+<AppProvider>
   <App />
+  </AppProvider>
 </BrowserRouter>
 )
